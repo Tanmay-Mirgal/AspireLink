bed57a6c58aa7eddc7a2603835a0a0a483f4e380
Tanmay-Mirgal <tanmaymirgal26@gmail.com>
2025-03-26 20:23:26 +0530

    signup and login ui done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home/Home';
+import Signup from "./pages/Signup/Signup"
+import Login from './pages/Login/Login';
 
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/signup" element={<Signup/>} />
+      <Route path="/login" element={<Login/>} />
 
     </Routes>
   );
