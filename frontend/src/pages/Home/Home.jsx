9b325949ddc427a84626a0961e05923043f226bb
adityashah2701 <adityashah2701@gmail.com>
2025-03-26 21:44:46 +0530

    sidebar fixed

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,4 +1,5 @@
 import LandingPage from '@/components/landingpage/LandingPage';
+import MinimalistSidebar from '@/components/sidebar/MinimalistSidebar';
 import React from 'react';
 
 
@@ -7,6 +8,7 @@ function Home() {
         <>
        
         <div className="flex">
+            <MinimalistSidebar/>
             <main className="flex-grow">
                 <LandingPage />
             </main>
