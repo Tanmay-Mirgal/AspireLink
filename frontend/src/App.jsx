bccf0a65c90b30a12e05e48ae380c0b709bd2363
Disha-patel-2709 <dishapatel0t52@gmail.com>
2025-03-26 23:46:04 +0530

    kuch toh kiya abhi

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,15 +5,22 @@ import Home from './pages/Home/Home';
 import Signup from "./pages/Signup/Signup";
 import Login from './pages/Login/Login';
 import Role from './pages/Role/Role';
+import StudentProfile from './pages/User/StudentProfilePage';
+import MentorProfile from './pages/Mentor/MentorProfile';
+import MinimalistSidebar from './components/sidebar/MinimalistSidebar';
 
 function App() {
   return (
     <>
+    <MinimalistSidebar/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
         <Route path="/role" element={<Role />} />
+        <Route path="/studentprofile" element={<StudentProfile />} />
+        <Route path="/mentorprofile" element={<MentorProfile />} />
+
       </Routes>
       <Toaster />
     </>
