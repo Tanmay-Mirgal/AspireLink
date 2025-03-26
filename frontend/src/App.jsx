14bb90733e8f43a55a1bc6136a3e30576a68008d
Tanmay-Mirgal <tanmaymirgal26@gmail.com>
2025-03-26 18:45:20 +0530

    routes added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,12 +5,10 @@ import Home from './pages/Home/Home';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-         <Route path="/" element={<Home />} />
-        
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+
+    </Routes>
   );
 }
 
