2fa08bfd286852541dfae39e81bbc6fd641aa6be
adityashah2701 <adityashah2701@gmail.com>
2025-03-28 12:29:26 +0530

    post controller done

--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -1,18 +1,28 @@
-import {Router} from "express"
-import { createPost, deletePost, getPost, getPostById } from "../controllers/post.controller.js";
-import { protectedRoute } from "../middlewares/auth.middleware.js";
+import { Router } from "express"
+import { 
+    createPost, 
+    deletePost, 
+    getAllPost, 
+    getPostById,
+    updatePost,
+    likePost,
+    addComment,
+    deleteComment
+} from "../controllers/post.controller.js"
+import { protectedRoute } from "../middlewares/auth.middleware.js"
 
-const router = Router();
+const router = Router()
 
-router.use(protectedRoute);
+router.use(protectedRoute)
 
-router.post('/create',createPost);
-router.get('/get',getPost);
-router.get('/get/:id',getPostById);
-router.delete('/delete/:id',deletePost);
+router.post('/create-post', createPost)
+router.get('/all-posts', getAllPost)
+router.get('/:id', getPostById)
+router.put('/update/:id', updatePost)
+router.delete('/delete/:id', deletePost)
+router.post('/like/:id', likePost)
+router.post('/:id/comment', addComment)
+router.delete('/:id/comment/:commentId', deleteComment)
 
 
-
-
-
-export default router;
+export default router
